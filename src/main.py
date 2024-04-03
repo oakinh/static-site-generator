4e4033d3bd56384d3c65b39ced446f1a50010415
oakinh <nikaohill@gmail.com>
2024-04-03 14:10:32 -0600

    Wrote unit tests, created TextNode class

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode
+
+def main():
+    test_TextNode = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(repr(test_TextNode))
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,28 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_2(self):
+        node = TextNode("This is my first test", "italics", "https://www.google.com")
+        node2 = TextNode("This is my first test", "italics", "https://www.google.com")
+        self.assertEqual(node, node2)
+
+    def test_3(self):
+        node = TextNode("This is my first test", "bold", "https://www.google.com")
+        node2 = TextNode("This is my first test", "italics", "https://www.google.com")
+        self.assertEqual(node, node2)
+    
+    def test_4(self):
+        node = TextNode("This is not my first test", "bold", "https://www.google.com")
+        node2 = TextNode("This is my first test", "italics", "https://www.google.com")
+        self.assertEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
